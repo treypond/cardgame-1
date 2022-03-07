bce3766497cbb7a84330b9b304372271589910d9
Isaac Sims <simsi1@mymail.nku.edu>
2022-03-07 00:03:48 -0500

    Try again to commit/push

---
+++ src/csc439teamllama/cardgame/playingCard.java
@@ -0,0 +1,245 @@
+package csc439teamllama.cardgame;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+/**
+ * A playing card
+ * <P>
+ *     Playing card has a number of methods, including getters and setters for determine suit, card number, and direction
+ *     it is facing. These values are stored as enums, with equivalent int values for each value, in the case of suit and
+ *     number, and boolean, in the case of if it is either facing up, or facing down. This is for ease of use of the user,
+ *     as well as significantly speeding up large scale manipulation, as seen with the create deck staic function, allowing
+ *     the creation of a deck with 52 cards face down, all unique with a very simple logic.
+ * </P>
+ * CSC-439
+ * @author Isaac Sims
+ * @version 1.0
+ */
+public class playingCard {
+    private Facing facing;
+    private Suit suit;
+    private Number number;
+
+    public playingCard(Facing facing, Suit suit, Number number) {
+        this.facing = facing;
+        this.suit = suit;
+        this.number = number;
+    }
+
+    public Facing getFacing() {
+        return facing;
+    }
+
+    public void setFacing(Facing facing) {
+        this.facing = facing;
+    }
+
+    public Suit getSuit() {
+        return suit;
+    }
+
+    public void setSuit(Suit suit) {
+        this.suit = suit;
+    }
+
+    public Number getNumber() {
+        return number;
+    }
+
+    public void setNumber(Number number) {
+        this.number = number;
+    }
+
+    /**
+     * Turns over the playing card
+     * <P>
+     *     flips playing card so that the other side is facing up.
+     * </P>
+     * CSC-439
+     * @author Isaac Sims
+     * @version 1.0
+     */
+    public void flipCard(){
+        facing = Facing.faceBoolOf(!getFacing().getFaceBool());
+    }
+
+    /**
+     * A method to generate a playing card deck with 52 cards
+     * <P>
+     *     creates a playing card deck with all possible unique combinations of suites and numbers, face down. Does so by
+     *     making extensive use of of the enum value maps created in our enum classes, further explained in the
+     *     enum documentation.
+     * </P>
+     * CSC-439
+     * @author Isaac Sims
+     * @version 1.0
+     * @return A deck of 52 playing cards
+     */
+    public static playingCard[] createDeck(){
+        playingCard deck[] = new playingCard[52];
+        for(int i = 0; i<4 ;i++){
+            for (int j = 0; j<13 ;j++){
+                deck[((i*13)+j)] = new playingCard(Facing.faceBoolOf(false),Suit.suitNumOf(i+1),Number.numOf(j+1));
+            }
+        }
+        return deck;
+    }
+
+    /**
+     * Enum, for the direction the card is lying, either face up, or face down.
+     * <P>
+     *     When the card is face down, it has an internal value of false, and when facing up, it has an internal value of true
+     * </P>
+     * CSC-439
+     * @author Isaac Sims
+     * @version 1.0
+     */
+    public enum Facing{
+        DOWN(false),UP(true);
+
+        private final boolean faceBool;
+        private final static Map map = new HashMap();
+
+        Facing(boolean faceBool){
+            this.faceBool = faceBool;
+        }
+
+        /**
+         * The section mapping each enum to it's boolean value, allowing us to convert the value to the enum in the
+         * upcoming method.
+         * CSC-439
+         * @author Isaac Sims
+         * @version 1.0
+         */
+        static {
+            for (Facing faces : Facing.values()) {
+                map.put(faces.faceBool, faces);
+            }
+        }
+
+        /**
+         * uses our hashmap to retrieve the proper enum for our entered value
+         * @param bool  This is the side the card is lying on, represented by a boolean false if the card face is on
+         *              facing down, and true if the card face is facing up.
+         * @return the correct enum for the supplied boolean value for the side the card is lying on
+         * @author Isaac Sims
+         * @version 1.0
+         */
+        public static Facing faceBoolOf(boolean bool) {
+            return (Facing) map.get(bool);
+        }
+
+        public boolean getFaceBool() {
+            return faceBool;
+        }
+    }
+
+    /**
+     * Enum, for the suit of each playing card: spades, diamonds, clubs, and hearts.
+     * <P>
+     *     Theses enums go up sequentially in value for the starting deck order, simplifies deck creation.
+     *     This also assists in deck creation and manipulation, and is achieved with a hash map mapping the specific
+     *     enum value to it's relative suit value so we can both get the number value as an int, but get the enum
+     *     value from the corresponding int.
+     * </P>
+     * CSC-439
+     * @author Isaac Sims
+     * @version 1.0
+     */
+    public enum Suit{
+        SPADES(1),DIAMONDS(2),CLUBS(3),HEARTS(4);
+
+        private final int suitNum;
+        private final static Map map = new HashMap();
+
+        Suit(int suitBool){
+            this.suitNum = suitBool;
+        }
+
+        /**
+         * The section mapping each enum to it's number value, allowing us to convert the value to the enum in the
+         * upcoming method.
+         * CSC-439
+         * @author Isaac Sims
+         * @version 1.0
+         */
+        static {
+            for (Suit suits : Suit.values()) {
+                map.put(suits.suitNum, suits);
+            }
+        }
+
+        /**
+         * uses our hashmap to retrieve the proper enum for our entered value
+         * @param num  This is the side the suit of the card, represented by the value of the suits in new deck order
+         *             starting with spade being represented by 4, until heart represented by 4
+         * @return the correct enum for the supplied boolean value for the side the card is lying on
+         * @author Isaac Sims
+         * @version 1.0
+         */
+        public static Suit suitNumOf(int num) {
+            return (Suit) map.get(num);
+        }
+
+        public int getSuitNum() {
+            return suitNum;
+        }
+    }
+
+
+    /**
+     * Enum, for the number of each card
+     * <P>
+     *     the number cards in order of value from 2 to 10, the face cards having values higher than 10 in order of
+     *     precedence, and ace placed as the first value, as it can be 1 or 14 and makes for proper new deck order.
+     *     This also assists in deck creation and manipulation, and is achieved with a hash map mapping the specific
+     *     enum value to it's relative number value so we can both get the number value as an int, but get the enum
+     *     value from the corresponding int.
+     * </P>
+     * CSC-439
+     * @author Isaac Sims
+     * @version 1.0
+     */
+    public enum Number{
+        ACE(1),TWO(2),THREE(3),FOUR(4),FIVE(5),SIX(6),SEVEN(7),
+        EIGHT(8),NINE(9),TEN(10),JACK(11),QUEEN(12),KING(13);
+
+        private final int cardNum;
+        private final static Map map = new HashMap();
+
+        Number(int cardNum){
+            this.cardNum = cardNum;
+        }
+
+        /**
+         * The section mapping each enum to it's number value, allowing us to convert the value to the enum in the
+         * upcoming method.
+         * CSC-439
+         * @author Isaac Sims
+         * @version 1.0
+         */
+        static {
+            for (Number num : Number.values()) {
+                map.put(num.cardNum, num);
+            }
+        }
+
+        /**
+         * uses our hashmap to retrieve the proper enum for our entered value
+         * @param num  This is the number of the card, represented by the value of the number of the card through
+         *             in increasing value, with 1 corresponding to one, and 13 corresponding to king.
+         * @return the correct enum for the supplied number value for number or face on the card
+         * @author Isaac Sims
+         * @version 1.0
+         */
+        public static Number numOf(int num) {
+            return (Number) map.get(num);
+        }
+
+        public int getNum() {
+            return cardNum;
+        }
+    }
+}
---
+++ tests/csc439teamllama/cardgame/playingCardTest.java
@@ -0,0 +1,62 @@
+package csc439teamllama.cardgame;
+
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.util.stream.IntStream;
+
+import static com.google.common.truth.Truth.*;
+
+class playingCardTest {
+    playingCard deck[];
+
+    @BeforeAll
+    @Test
+    void testCreatedeck(){
+        deck = playingCard.createDeck();
+        IntStream.range(0, 52).forEach(i -> assertThat(deck[i].getFacing()).isEqualTo(playingCard.Facing.DOWN));
+        for(int i = 0;i<13;i++){
+
+        }
+    }
+
+    @BeforeEach
+    void setDeck(){
+        deck =  playingCard.createDeck();
+    }
+
+    @Test
+    void testFlip(){
+        assertThat(deck[0].getFacing().getFaceBool()).isEqualTo(playingCard.Facing.DOWN);
+        deck[0].flipCard();
+        assertThat(deck[0].getFacing().getFaceBool()).isEqualTo(playingCard.Facing.UP);
+        deck[0].flipCard();
+        assertThat(deck[0].getFacing().getFaceBool()).isEqualTo(playingCard.Facing.DOWN);
+    }
+
+    @org.junit.jupiter.api.Test
+    void getFacing() {
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void setFacing() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getSuit() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setSuit() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getNumber() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setNumber() {
+    }
+}
