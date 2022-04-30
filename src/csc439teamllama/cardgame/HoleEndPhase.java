224b1831f5758001b9eda0d04d76c67a447ce3da
Trey Pond <treypond13@gmail.com>
2022-04-29 20:59:53 -0400

    Created HoleEndPhase class.

---
+++ src/csc439teamllama/cardgame/HoleEndPhase.java
@@ -0,0 +1,28 @@
+package csc439teamllama.cardgame;
+
+import java.util.Collections;
+
+/**
+ * This class collect all players cards,
+ * return them to the deck, shuffle the deck, and add new cards to players hands.
+ */
+public class HoleEndPhase implements Phase {
+    @Override
+    public void execute(GolfGameModel game, GolfView view ){
+         // game.scoreBoardUpdate();
+
+          game.deck.addAll(game.discard);
+          for(int i=0; i<game.players.length;i++){
+              Collections.addAll(game.deck, game.players[i].hand);
+          }
+
+          Collections.shuffle(game.deck);
+
+          for(int i=0; i<game.players.length;i++){
+              for(int j=0; j<6; j++ ){
+                  game.players[i].hand[j]=game.deck.remove(game.deck.size()-1);
+              }
+          }
+       game.phase= new DrawPhase();
+    }
+}
