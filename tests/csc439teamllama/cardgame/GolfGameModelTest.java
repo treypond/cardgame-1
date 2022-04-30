40adaa631ca63c16dfaf77544e76fcb58adb1d9c
jaggersb1 <101071422+jaggersb1@users.noreply.github.com>
2022-04-30 19:24:06 -0400

    Added a test for the scoreboardUpdate method.

--- tests/csc439teamllama/cardgame/GolfGameModelTest.java
+++ tests/csc439teamllama/cardgame/GolfGameModelTest.java
@@ -19,4 +19,69 @@ class GolfGameModelTest {
         game.turn = 25;
         assertThat(game.playerIndex()).isEqualTo(0);
     }
+
+    @Test
+    void scoreboardUpdate() {
+        GolfGameModel game = new GolfGameModel(4, 4);
+
+        game.players[0] = new GolfPlayerModel("Joe");
+        game.players[1] = new GolfPlayerModel("Jay");
+        game.players[2] = new GolfPlayerModel("Josh");
+        game.players[3] = new GolfPlayerModel("Jameson");
+
+        //Score for this player should be 3
+        game.players[0].hand = new playingCard[]{
+                new playingCard(playingCard.Facing.UP, playingCard.Suit.SPADES, playingCard.Number.KING),
+                new playingCard(playingCard.Facing.UP, playingCard.Suit.SPADES, playingCard.Number.KING),
+                new playingCard(playingCard.Facing.UP, playingCard.Suit.SPADES, playingCard.Number.KING),
+                new playingCard(playingCard.Facing.UP, playingCard.Suit.SPADES, playingCard.Number.ACE),
+                new playingCard(playingCard.Facing.UP, playingCard.Suit.SPADES, playingCard.Number.ACE),
+                new playingCard(playingCard.Facing.UP, playingCard.Suit.SPADES, playingCard.Number.ACE)
+        };
+
+        //Score for this player should be 30
+        game.players[1].hand = new playingCard[]{
+                new playingCard(playingCard.Facing.UP, playingCard.Suit.SPADES, playingCard.Number.KING),
+                new playingCard(playingCard.Facing.UP, playingCard.Suit.SPADES, playingCard.Number.KING),
+                new playingCard(playingCard.Facing.UP, playingCard.Suit.SPADES, playingCard.Number.KING),
+                new playingCard(playingCard.Facing.UP, playingCard.Suit.SPADES, playingCard.Number.JACK),
+                new playingCard(playingCard.Facing.UP, playingCard.Suit.SPADES, playingCard.Number.JACK),
+                new playingCard(playingCard.Facing.UP, playingCard.Suit.SPADES, playingCard.Number.JACK)
+        };
+
+        //Score for this player should be 6
+        game.players[2].hand = new playingCard[]{
+                new playingCard(playingCard.Facing.UP, playingCard.Suit.SPADES, playingCard.Number.KING),
+                new playingCard(playingCard.Facing.UP, playingCard.Suit.SPADES, playingCard.Number.KING),
+                new playingCard(playingCard.Facing.UP, playingCard.Suit.SPADES, playingCard.Number.KING),
+                new playingCard(playingCard.Facing.UP, playingCard.Suit.SPADES, playingCard.Number.QUEEN),
+                new playingCard(playingCard.Facing.UP, playingCard.Suit.SPADES, playingCard.Number.TWO),
+                new playingCard(playingCard.Facing.UP, playingCard.Suit.SPADES, playingCard.Number.TWO)
+        };
+
+        //Score for this player should be 0
+        game.players[3].hand = new playingCard[]{
+                new playingCard(playingCard.Facing.UP, playingCard.Suit.SPADES, playingCard.Number.JACK),
+                new playingCard(playingCard.Facing.UP, playingCard.Suit.SPADES, playingCard.Number.QUEEN),
+                new playingCard(playingCard.Facing.UP, playingCard.Suit.SPADES, playingCard.Number.ACE),
+                new playingCard(playingCard.Facing.UP, playingCard.Suit.SPADES, playingCard.Number.JACK),
+                new playingCard(playingCard.Facing.UP, playingCard.Suit.SPADES, playingCard.Number.QUEEN),
+                new playingCard(playingCard.Facing.UP, playingCard.Suit.SPADES, playingCard.Number.ACE)
+        };
+
+
+        game.scoreboardUpdate();
+
+        assertEquals(game.players[0].score, 3);
+        assertEquals(game.players[1].score, 30);
+        assertEquals(game.players[2].score, 6);
+        assertEquals(game.players[3].score, 0);
+
+        assertEquals(game.scoreBoard[0], game.players[3]);
+        assertEquals(game.scoreBoard[1], game.players[0]);
+        assertEquals(game.scoreBoard[2], game.players[2]);
+        assertEquals(game.scoreBoard[3], game.players[1]);
+
+
+    }
 }
